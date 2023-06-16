ce436d4fad2fa574b90f4b2fb1712932e118c672
ODUOLA Ayodele J <a024459m@student.staffs.ac.uk>
2023-06-16 17:41:59 +0100

    completed signin and signup pages

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,9 +1,39 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import '../components/styles/signin.css'
 const Signin = () => {
   return (
-    <div>
-     Signin
+    <div id='container' className='d-flex justify-content-center align-items-center'>
+      <section className='container'>
+        <div className='row'>
+          <aside className='col-lg-6 d-flex justify-content-center' >
+            <img alt='BankImage' width="90%" src ="https://img.freepik.com/premium-vector/bank-building-online-banking-finance-bank-transactions-bank-service-3d-vector-icon-cartoon-minimal-style_365941-689.jpg?w=826"/>
+          </aside>
+          <aside className='col-lg-6 d-flex justify-content-center align-items-center loginsection'>
+            <section >
+          <h2>KLG</h2>
+        <h5><strong>Hey, hello</strong> 
+        <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m434-498 276-275q9-9 21-9t21 9q9 9 9 21t-9 21L476-455l-42-43Zm104 104 247-247q9.067-9 21.533-9Q819-650 828-641.158q9 8.842 9 21T828-599L580-351l-42-43ZM195-199q-91-91-91.5-218T194-635l122-122 42 42q12 12 19.5 28t8.5 30l163-164q9.067-9 21.533-9Q583-830 592-820.947q9 9.052 9 21.5Q601-787 592-778L389-575l-65 65 26 26q42 42 40.5 100.5T347-283l-7 7-42-43 7-7q25-25 25.5-58.5T307-442l-47-47q-9-9.067-9-21.533Q251-523 260-532l56-55q17-17 17-42.5T316-672l-80 80q-73 73-72.5 175T237-242q74 74 177 75.5T590-238l241-241q9.067-9 21.533-9Q865-488 874-479.158q9 8.842 9 21T874-437L633-196q-91 91-219 89.5T195-199Zm217-217ZM689-40l1-60q71 0 120.5-49.5T860-270l60-1q0 95.851-67.575 163.425Q784.851-40 689-40ZM40-689q0-95.851 67.575-163.425Q175.149-920 271-920l-1 60q-71 0-120.5 49.5T100-690l-60 1Z"/></svg>
+        </h5>
+        <p className='text-muted'>Enter the information you entered while Registering</p>
+        <form>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+          </div>
+          <span className="d-flex justify-content-end">Forgot Password?</span>
+          <span><input id='loginbtn' className=' btn btn-primary ' type="submit" value="Login" /></span>
+        </form>
+        <p>Don't have an Account? <Link id='signuplinkk' to="/Signup">Signup</Link></p>
+        </section>
+          </aside>
+        </div>
+        
+      </section>
     </div>
   )
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,59 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import '../components/styles/signin.css'
 const Signup = () => {
   return (
-    <div>
-     Signup
+    <div id='container' className='d-flex justify-content-center align-items-center'>
+      <section className='container'>
+        <div className='row'>
+          <aside className='col-lg-6 d-flex justify-content-center' >
+            <img alt='BankImage' width="90%" src="https://img.freepik.com/premium-vector/bank-building-online-banking-finance-bank-transactions-bank-service-3d-vector-icon-cartoon-minimal-style_365941-689.jpg?w=826" />
+          </aside>
+          <aside className='col-lg-6 d-flex justify-content-center align-items-center signupsection '>
+            <section >
+              <h2>Create a new KLG Account</h2>
+
+              <form>
+                <div className="mb-3 d-flex">
+                  <div className="me-3">
+                    <label htmlFor="firstNameInput" className="form-label">First Name</label>
+                    <input type="text" className="form-control" id="firstNameInput" placeholder="First Name" />
+                  </div>
+                  <div>
+                    <label htmlFor="lastNameInput" className="form-label">Last Name</label>
+                    <input type="text" className="form-control" id="lastNameInput" placeholder="Last Name" />
+                  </div>
+                </div>
+                <div className="mb-3">
+
+                  <label htmlFor="username" className="form-label">User Name</label>
+                  <input type="text" className="form-control" id="username" placeholder="Username" />
+
+                </div>
+               
+                <div className="mb-3">
+                  <label htmlFor="emailInput" className="form-label">Email address</label>
+                  <input type="email" className="form-control" id="emailInput" placeholder="name@example.com" />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="passwordInput" className="form-label">Password</label>
+                  <input type="password" className="form-control" id="passwordInput" placeholder="Password" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="passwordInput" className="form-label">Confirm Password</label>
+                  <input type="password" className="form-control" id="passwordInput" placeholder="Password" />
+                </div>
+                <span className="d-flex justify-content-end">Forgot Password?</span>
+                 <span><input id='signupbtn' className=' btn btn-primary ' type="submit" value="Signup" /></span>
+              </form>
+
+              <p>Already have an Account? <Link id='signuplinkk' to="/Signin">Signin</Link></p>
+            </section>
+          </aside>
+        </div>
+
+      </section>
     </div>
   )
 }
